450902b12cbd54ad4a9a6f8b2f0a852227e0905d
DarmoyanHam <138680927+DarmoyanHam@users.noreply.github.com>
2023-07-16 19:12:08 +0400

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%i", &a);
+    if(a % 4 == 0)
+    {
+        printf("Nahanj e\n");
+    }
+    else
+        printf("Nahanj che\n");
+
+    return 0;
+}
